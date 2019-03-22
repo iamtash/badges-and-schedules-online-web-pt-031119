06a6e9b2f8a02283a84229039fdfbcad753cd3fa
Natasha Chernis <natashachernis@gmail.com>
2019-03-22 21:18:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,6 @@ def printer(attendees)
   #room_assignments = assign_rooms(attendees)
   badge_messages(attendees).each {|badge_message| puts badge_message}
 
-  end
   room_assignments.each do |room_assignment|
     puts room_assignment
   end
